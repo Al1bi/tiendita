715e389762657f2960048cd3b66930a51c09b8ec
Al1bi <denilson.fiesta@ucb.edu.bo>
2024-05-27 15:29:45 -0400

    primera parte del carrusel

--- src/app/elementos/producto/producto.component.ts
+++ src/app/elementos/producto/producto.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Producto } from '../../interfaces/product';
 
 @Component({
   selector: 'app-producto',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './producto.component.html',
   styleUrl: './producto.component.scss'
 })
 export class ProductoComponent {
   @Input() producto!: Producto;
+  stars: number[] = [1, 2, 3, 4, 5];
 }
