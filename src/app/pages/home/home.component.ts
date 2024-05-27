85d13f4593777c19b8cad03ed7b92a45c94dd0a9
Al1bi <denilson.fiesta@ucb.edu.bo>
2024-05-26 21:26:40 -0400

    Trabajo combinado

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
-import {ProductoComponent} from "../../elementos/producto/producto.component";
-import {Producto} from "../../interfaces/product";
-import {ProductoService} from "../../servicios/producto.service";
+import { ProductoComponent } from "../../elementos/producto/producto.component";
+import { Producto } from "../../interfaces/product";
+import { ProductoService } from "../../servicios/producto.service";
 
 
-  @Component({
+import { map } from 'rxjs';
+
+@Component({
   selector: 'app-tienda',
   standalone: true,
   imports: [
@@ -13,12 +15,25 @@ import {ProductoService} from "../../servicios/producto.service";
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
+
 export class HomeComponent {
   listaDeProductos: Producto[] = [];
   productoService: ProductoService = inject(ProductoService);
   constructor() {
-    this.productoService.obtenerTodosLosProductos().subscribe(
+    // this.productoService.obtenerTodosLosProductos().subscribe(
+    //   data => this.listaDeProductos = data
+    // )
+    this.productoService.obtenerTodosLosProductos().pipe(
+      map(productos => {
+        const totalProductos = productos.length;
+        if (totalProductos >= 8) {
+          return [...productos.slice(0, 4), ...productos.slice(-4)];
+        } else {
+          return [];
+        }
+      })
+    ).subscribe(
       data => this.listaDeProductos = data
-    )
+    );
   }
 }
