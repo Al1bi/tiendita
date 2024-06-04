1fb18403f153531d5396d268f181b6244a418f2d
Al1bi <denilson.fiesta@ucb.edu.bo>
2024-06-04 15:17:21 -0400

    commitsito 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,8 @@ import { Routes } from '@angular/router';
 import { CarritoComponent } from './elementos/carrito/carrito.component';
 import { CategoryComponent } from './pages/category/category.component';
 import { DetallesComponent } from './pages/detalles/detalles.component';
-import { ElectronicsComponent } from './pages/electronics/electronics.component';
 import { HomeComponent } from './pages/home/home.component';
-import { JewelwryComponent } from './pages/jewelwry/jewelwry.component';
-import { MenComponent } from './pages/men/men.component';
 import { NewProductComponent } from './pages/new-product/new-product.component';
-import { WomenComponent } from './pages/women/women.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -15,9 +11,5 @@ export const routes: Routes = [
   { path: 'detalles/:id', component: DetallesComponent },
   { path: 'category/:categoria', component: CategoryComponent},
   { path: 'carrito', component: CarritoComponent },
-  { path: 'men', component: MenComponent },
-  { path: 'women', component: WomenComponent },
-  { path: 'electronics', component: ElectronicsComponent },
-  { path: 'jewelery', component: JewelwryComponent },
   { path: 'new-product', component: NewProductComponent }
 ];
