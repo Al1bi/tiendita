f31eac1019280e4cb7d24ef31b89d6c9994b6f09
Al1bi <denilson.fiesta@ucb.edu.bo>
2024-06-04 08:47:01 -0400

    nose

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,8 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ProductoComponent } from "../../elementos/producto/producto.component";
 import { Producto } from "../../interfaces/product";
 import { ProductoService } from "../../servicios/producto.service";
-import { CommonModule } from '@angular/common';
 
 
 import { map } from 'rxjs';
@@ -21,10 +21,19 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 
 export class HomeComponent {
   listaDeProductos: Producto[] = [];
-  categorias: string[] = ['All', 'women\'s clothing', 'men\'s clothing', 'jewelery', 'electronics'];
+  categorias: string[] = ["All", "Women's", "Men's", "Jewelry", "Electronics"];
   categoriaSeleccionada: string = 'All';
   productoService: ProductoService = inject(ProductoService);
+
+  mp: Map<string, string> = new Map();
+
   constructor() {
+
+    this.mp.set("Women's", "women\'s clothing");
+    this.mp.set("Men's", "men\'s clothing");
+    this.mp.set("Jewelry", "jewelery");
+    this.mp.set("Electronics", "electronics");
+
     // this.productoService.obtenerTodosLosProductos().subscribe(
     //   data => this.listaDeProductos = data
     // )
@@ -47,7 +56,19 @@ export class HomeComponent {
   }
 
   obtenerProductos(): void {
-    this.productoService.obtenerTodosLosProductos().subscribe(
+    // this.productoService.obtenerTodosLosProductos().subscribe(
+    //   data => this.listaDeProductos = data
+    // );
+    this.productoService.obtenerTodosLosProductos().pipe(
+      map(productos => {
+        const totalProductos = productos.length;
+        if (totalProductos >= 8) {
+          return [...productos.slice(0, 4), ...productos.slice(-4)];
+        } else {
+          return [];
+        }
+      })
+    ).subscribe(
       data => this.listaDeProductos = data
     );
   }
@@ -57,7 +78,10 @@ export class HomeComponent {
     if (categoria === 'All') {
       this.obtenerProductos();
     } else {
-      this.productoService.obtenerProductosPorCategoria(categoria).subscribe(
+      
+      let value: string = this.mp.get(categoria) ?? 'All';
+
+      this.productoService.obtenerProductosPorCategoria(value).subscribe(
         data => this.listaDeProductos = data
       );
     }
